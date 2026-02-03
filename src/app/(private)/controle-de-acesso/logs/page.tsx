b225b7c1099640932307a2830c6213e03f4afcf8
ManuelCyrus <manuelcyrus.mda@gmail.com>
2026-02-03 18:04:37 +0000

    dev

--- src/app/(private)/controle-de-acesso/logs/page.tsx
+++ src/app/(private)/controle-de-acesso/logs/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
 import { useInvoice } from "@/services/invoices";
 
 interface Log {
